742116a3ba595a444de61d8bdf17a1ba8b0f2f52
meghansh <mgoel7@illinois.edu>
2022-12-03 01:12:48 -0600

    fix 1

--- config.py
+++ config.py
@@ -29,7 +29,8 @@ INTRODUCER_DNS_PORT = 8888
 USERNAME = "mgoel7"
 PASSWORD = None
 
-with open('./password.txt') as f:
+# with open('./password.txt') as f:
+with open('/home/bachina3/MP3/awesomesdfs/password.txt')
     line = f.readline()
     USERNAME = line.split(',')[0].strip()
     PASSWORD = line.split(',')[1].strip()
