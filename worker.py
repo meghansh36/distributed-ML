27b5e1d1b9665ebb8eeab7032aa997c168e5923d
rahulbachina <rahulbachina11@gmail.com>
2022-12-03 19:42:32 -0600

    test 51

--- worker.py
+++ worker.py
@@ -55,16 +55,16 @@ class Worker:
         self.model_dict = {
             "InceptionV3": {
                 'hyperparams' : {
-                    'batch_size' : 10, 
-                    'time': ModelParameters(download_time=1, model_load_time=5.6, first_image_predict_time=2, each_image_predict_time=0.325, batch_size=10).execution_time_per_vm()
+                    'batch_size' : 20, 
+                    'time': ModelParameters(download_time=1, model_load_time=5.6, first_image_predict_time=2, each_image_predict_time=0.325, batch_size=20).execution_time_per_vm()
                     },
                 'queue': [],
                 'inprogress_queue': []
             },
             "ResNet50": {
                 'hyperparams' : {
-                    'batch_size' : 20, 
-                    'time': ModelParameters(download_time=1, model_load_time=3.5, first_image_predict_time=1, each_image_predict_time=0.250, batch_size=20).execution_time_per_vm()
+                    'batch_size' : 10, 
+                    'time': ModelParameters(download_time=1, model_load_time=3.5, first_image_predict_time=1, each_image_predict_time=0.250, batch_size=10).execution_time_per_vm()
                     },
                 'queue': [],
                 'inprogress_queue': []
